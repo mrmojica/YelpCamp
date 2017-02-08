b33c4b70117494f897bafe4eac50293f58d88f9c
mrmojica <mrmojica510@gmail.com>
2017-02-08 10:00:50 -0800

    + landing page/header/footer

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res){
+	var campgrounds = [
+		{name: "Big Basin", image: "https://farm7.staticflickr.com/6188/6106475454_cf4dab4d64.jpg"},
+		{name: "Lake Ford", image: "https://farm4.staticflickr.com/3189/3062178880_4edc3b60d5.jpg"},
+		{name: "Green Hill", image: "https://farm4.staticflickr.com/3149/3062180144_ee0d2d466a.jpg"}
+	];
+
+	res.render("campgrounds", {campgrounds: campgrounds});
 
 });
 
