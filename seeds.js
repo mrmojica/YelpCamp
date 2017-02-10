73a8de06ed9db5baace49ec99a222d726d5692b1
mrmojica <mrmojica510@gmail.com>
2017-02-09 18:35:23 -0800

    applied bootstrap layout show.ejs-info/comment

--- seeds.js
+++ seeds.js
@@ -6,19 +6,19 @@ let data = [
 	{
 		name: "Oakes Rest",
 		image: "https://farm9.staticflickr.com/8590/16684265096_9bf65a3440.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet t-bone turducken corned beef pastrami meatloaf cow chicken short ribs ball tip jowl. Frankfurter shank bresaola kielbasa meatloaf ball tip beef picanha pancetta porchetta. Alcatra porchetta ham hamburger pork loin biltong. Leberkas drumstick rump tail. Burgdoggen shoulder tenderloin ground round porchetta, fatback tri-tip biltong turducken frankfurter leberkas pork chop beef ribs. Ham drumstick jerky boudin, porchetta kevin sausage. Shank pork loin jerky chuck alcatra."
 
 	},
 	{
 		name: "Lake Rest",
 		image: "https://farm1.staticflickr.com/150/333980768_ab02f4e32c.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet t-bone turducken corned beef pastrami meatloaf cow chicken short ribs ball tip jowl. Frankfurter shank bresaola kielbasa meatloaf ball tip beef picanha pancetta porchetta. Alcatra porchetta ham hamburger pork loin biltong. Leberkas drumstick rump tail. Burgdoggen shoulder tenderloin ground round porchetta, fatback tri-tip biltong turducken frankfurter leberkas pork chop beef ribs. Ham drumstick jerky boudin, porchetta kevin sausage. Shank pork loin jerky chuck alcatra."
 
 	},
 	{
 		name: "Green Rest",
 		image: "https://farm3.staticflickr.com/2363/2386955872_a1196b0286.jpg",
-		description: "blah blah blah"
+		description: "Bacon ipsum dolor amet t-bone turducken corned beef pastrami meatloaf cow chicken short ribs ball tip jowl. Frankfurter shank bresaola kielbasa meatloaf ball tip beef picanha pancetta porchetta. Alcatra porchetta ham hamburger pork loin biltong. Leberkas drumstick rump tail. Burgdoggen shoulder tenderloin ground round porchetta, fatback tri-tip biltong turducken frankfurter leberkas pork chop beef ribs. Ham drumstick jerky boudin, porchetta kevin sausage. Shank pork loin jerky chuck alcatra."
 
 	}
 ]
