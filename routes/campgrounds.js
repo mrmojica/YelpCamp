97e73c4e133a272143224f0f3deb8e8c67c17044
mrmojica <mrmojica510@gmail.com>
2017-02-12 19:39:47 -0800

    added edit/delete routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -83,6 +83,21 @@ router.put("/campgrounds/:id", function(req, res){
 });
 
 
+// DELETE CAMPGROUND
+
+router.delete("/campgrounds/:id", function(req, res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+
+
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
