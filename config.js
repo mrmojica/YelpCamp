93885b6ede158b9711d16f499125cce01d3f6059
mrmojica <mrmojica510@gmail.com>
2017-02-20 20:05:05 -0800

    initial deploy

---
+++ config.js
@@ -0,0 +1,5 @@
+// exports.DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL ||
+//                        (process.env.NODE_ENV = 'mongodb://campground:campground123@ds157349.mlab.com:57349/campgrounds');
+// exports.PORT = process.env.PORT || 8080;
+
