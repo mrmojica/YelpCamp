b0adac6bb5fcb21dc96f33ac3577a82190fee247
mrmojica <mrmojica510@gmail.com>
2017-02-20 20:38:28 -0800

    removed config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// exports.DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL ||
-//                        (process.env.NODE_ENV = 'mongodb://campground:campground123@ds157349.mlab.com:57349/campgrounds');
-// exports.PORT = process.env.PORT || 8080;
-
